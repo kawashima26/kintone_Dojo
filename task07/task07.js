c4e61c1e5f39b7280210346eb5b1cb1a40c3ba91
kawashima26 <135286052+kawashima26@users.noreply.github.com>
2023-06-27 11:13:16 +0900

    Update task07.js
    変更点は以下になります。
    - キャメルケースに従って変数名を変更
    - 確認用のconsole.logを削除
    - 文字列結合の処理を行うイベントと、disabled にするイベントを分ける
    - イベント名の追加

--- task07/task07.js
+++ task07/task07.js
@@ -1,18 +1,19 @@
 (() => {
   'use strict';
-  const eventtype = ['app.record.create.show', 'app.record.edit.show',
-    'app.record.edit.change.日付', 'app.record.create.change.サイボウズ製品', 'app.record.create.change.管理番号'];
+  const eventType1 = ['app.record.create.show', 'app.record.edit.show'];
+  const eventType2 = ['app.record.edit.change.日付', 'app.record.edit.change.サイボウズ製品', 'app.record.edit.change.管理番号',
+    'app.record.create.change.日付', 'app.record.create.change.サイボウズ製品', 'app.record.create.change.管理番号'];
 
-  kintone.events.on(eventtype, (event) => {
-    console.log(event); // アプリのデータオブジェクト確認
-    const FieldElement = event.record.重複禁止項目_文字列; // 重複禁止項目フィールドを参照
-
+  kintone.events.on(eventType1, (event) => {
     // 重複禁止項目⽂字列フィールドは常に編集不可にする
-    FieldElement.disabled = true;
+    event.record.重複禁止項目_文字列.disabled = true;
+    return event;
+  });
 
+  kintone.events.on(eventType2, (event) => {
     // 日重複禁止項目文字列フィールドに入れる日付の値を定義
     const inputDate = event.record.日付.value;
-    const newdate = dateFns.format(inputDate, 'YYYYMMDD'); // 日付をハイフンなしの形に変換
+    const newDate = dateFns.format(inputDate, 'YYYYMMDD'); // 日付をハイフンなしの形に変換
 
     // サイボウズ製品の種類が選択されたとき、重複禁止項目文字列フィールドに入れる値を定義
     let proElement = '';
@@ -34,7 +35,7 @@
     // 管理番号を入力したとき、重複禁止項目文字列フィールドに入れる管理番号の値を定義
     const inputNumber = event.record.管理番号.value;
     // 重複禁止項目文字列に値を代入
-    FieldElement.value = `${newdate}-${proElement}-${inputNumber}`;
+    event.record.重複禁止項目_文字列.value = `${newDate}-${proElement}-${inputNumber}`;
     return event;
   });
 })();
