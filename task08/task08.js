d7dd04390d7d52aafdd88d8bb7f7e702146ae03e
kawashima26 <135286052+kawashima26@users.noreply.github.com>
2023-06-28 09:41:03 +0900

    Update task08.js
    以下の部分を変更しました
    - 関数getApiを削除
    - Action5のオブジェクトデータを配列にする作業とindex番号順に並び替えする作業をmap()でまとめて行う

--- task08/task08.js
+++ task08/task08.js
@@ -2,30 +2,19 @@
   'use strict';
   // イベント内容：「レコード追加画面を表示したとき」
   kintone.events.on('app.record.create.show', async (event) => {
-    // アプリのデータを取ってくるための関数getApi
-    const getApi = async () => {
-      const body = {
-        app: kintone.app.getId(), // 「id:アプリ番号」のコードと同じ
-      };
-      const resp = await kintone.api(kintone.api.url('/k/v1/app/form/fields.json', true), 'GET', body);
-      return resp;
+    // アプリのデータを取得
+    const body = {
+      app: kintone.app.getId(), // 「id:アプリ番号」のコードと同じ
     };
-    // 取得したアプリデータを変数で定義
-    const appData = await getApi();
+    const appData = await kintone.api(kintone.api.url('/k/v1/app/form/fields.json', true), 'GET', body);
 
     // アプリデータでAction5が入っているデータ
     const action5Data = appData.properties.Table.fields.Action5.options;
 
     const tableElement = event.record.Table.value; // テーブルの行全体を参照
 
-    // Action5のオブジェクトデータを配列にする
-    const action5DataSort = [];
-    Object.keys(action5Data).forEach((val) => {
-      action5DataSort.push(action5Data[val]);
-    });
-
-    // Action5のオブジェクトデータをindex番号順に並び替え
-    action5DataSort.sort((a, b) => {
+    // map()を使い、「Action5のオブジェクトデータを配列にする」と「Action5のオブジェクトデータをindex番号順に並び替え」を一度に行う
+    const action5DataSort = Object.keys(action5Data).map((val) => action5Data[val]).sort((a, b) => {
       if (a.index > b.index) {
         return 1;
       }
