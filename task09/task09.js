91b83e1fbb8c4f7e6061cd767b010e8139a7d6aa
kawashima26 <135286052+kawashima26@users.noreply.github.com>
2023-07-07 15:00:47 +0900

    Update task09.js
    【変更点】
    queryの部分を追加しました。

--- task09/task09.js
+++ task09/task09.js
@@ -3,12 +3,17 @@
   // レコード追加画面、レコード編集画面で保存ボタンを押したとき
   kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], async (event) => {
     // 他レコードの情報を取得
+    console.log(event);
+    const inputElement = event.record.重複禁止項目.value;
+    const recordNum = event.record.$id.value; // レコード編集画面で現在開いているレコードIDを取得（レコード編集画面だとeventオブジェクトに$idが表示される。レコード新規追加画面だと出ない）
+    // recordNumがあるか? あった場合、重複禁止項目の文字列取得and自分が現在開いているレコード番号は除く : なかった場合、重複禁止項目の文字列取得
+    const query = recordNum ? `重複禁止項目 = "${inputElement}" and $id != "${recordNum}"` : `重複禁止項目 = "${inputElement}"`;
     const body = {
       app: kintone.app.getId(),
+      query: query,
     };
     return kintone.api(kintone.api.url('/k/v1/records', true), 'GET', body).then((recordData) => {
       // 他レコードの重複禁止項目の要素を取得し配列に入れる
-      const inputElement = event.record.重複禁止項目.value;
       const recordElementArr = [];
       Object.keys(recordData.records).forEach((val) => {
         const recordElement = recordData.records[val].重複禁止項目.value;
